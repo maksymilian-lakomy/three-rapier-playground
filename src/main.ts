9814c0577b5994b46f93d776c474feb357d21e97
Maksymilian Łakomy <kontakt@maksymilianlakomy.pl>
2024-06-09 21:23:08 +0200

    feat: player controller

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import {Pane} from "tweakpane";
 let container: HTMLElement;
 
 let freeRoamCamera: THREE.PerspectiveCamera;
+let activeCamera: THREE.PerspectiveCamera;
 let orbitControls: OrbitControls;
 
 let scene: THREE.Scene;
@@ -24,7 +25,8 @@ let pane: Pane;
 
 const params = {
     directionalLightHelperVisible: false,
-    rapierDebugVisible: false
+    rapierDebugVisible: false,
+    mouseSensitivity: 200
 }
 
 let animationId: number | null = null;
@@ -60,9 +62,16 @@ async function init(): Promise<void> {
     const directionalLightHelperVisibleBinding = pane.addBinding(params, 'directionalLightHelperVisible')
     directionalLightHelperVisibleBinding.on('change', () => directionalLightHelper.visible = params.directionalLightHelperVisible);
 
+    pane.addBinding(params, 'mouseSensitivity', {
+        min: 100,
+        max: 500
+    });
+
     freeRoamCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
     freeRoamCamera.position.set(0, 4, 10);
     freeRoamCamera.lookAt(0, 1, 0);
+
+    activeCamera = freeRoamCamera;
 
     scene = new THREE.Scene();
     scene.background = hdrMap;
@@ -105,8 +114,8 @@ async function init(): Promise<void> {
 
     scene.add(level);
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    directionalLight.position.set( 10, 10,  10 );
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(10, 10, 10);
     directionalLight.castShadow = true;
     directionalLight.shadow.camera.top = 128;
     directionalLight.shadow.camera.bottom = -128;
@@ -118,11 +127,14 @@ async function init(): Promise<void> {
     directionalLight.shadow.mapSize.width = 2048;
     directionalLight.shadow.mapSize.height = 2048;
 
-    scene.add( directionalLight );
+    scene.add(directionalLight);
 
-    directionalLightHelper = new THREE.DirectionalLightHelper( directionalLight );
-    directionalLightHelper.visible = params.rapierDebugVisible;
+    directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+    directionalLightHelper.visible = params.directionalLightHelperVisible;
     scene.add(directionalLightHelper);
+
+    const axesHelper = new THREE.AxesHelper(2);
+    scene.add(axesHelper);
 
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -149,17 +161,171 @@ async function init(): Promise<void> {
     animationId = requestAnimationFrame(animate);
 }
 
+let playerInput = {
+    forward: 0,
+    backward: 0,
+    left: 0,
+    right: 0,
+    jump: 0
+}
+
+let playerEuler = new THREE.Euler(0, 0, 0);
+
+let playerCamera: THREE.PerspectiveCamera;
+let playerGroup: THREE.Group;
+
+let playerCollider: RAPIER.Collider
+let characterController: RAPIER.KinematicCharacterController
+
+const PLAYER_OFFSET = 0.01;
+
+// const PLAYER_CAMERA_SENSITIVITY = 150;
+const PLAYER_EVE_LEVEL = 1.8;
+
+const PLAYER_SPEED = 1;
+const PLAYER_COLLIDER_HEIGHT = 2;
+const PLAYER_COLLIDER_RADIUS = 0.4;
+
+function player(): void {
+    // THREE SETUP
+    playerGroup = new THREE.Group();
+
+    playerCamera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.01, 100);
+    playerCamera.position.set(0, PLAYER_EVE_LEVEL, 0);
+
+    playerGroup.add(playerCamera);
+    activeCamera = playerCamera
+    scene.add(playerGroup);
+
+    // RAPIER SETUP
+    const colliderDesc = RAPIER.ColliderDesc.cylinder(PLAYER_COLLIDER_HEIGHT / 2, PLAYER_COLLIDER_RADIUS);
+    colliderDesc.setTranslation(0, PLAYER_COLLIDER_HEIGHT / 2, 0);
+
+    playerCollider = physicsWorld.createCollider(colliderDesc);
+
+    characterController = physicsWorld.createCharacterController(PLAYER_OFFSET);
+    characterController.setUp({x: 0, y: 1, z: 0})
+    characterController.enableAutostep(1.1, 0.5, true);
+    characterController.enableSnapToGround(0.4);
+    characterController.setMinSlopeSlideAngle(30 * THREE.MathUtils.DEG2RAD);
+    characterController.setMaxSlopeClimbAngle(30 * THREE.MathUtils.DEG2RAD);
+
+    // EVENTS SETUP
+    container.addEventListener('click', () => {
+        if (activeCamera !== playerCamera) return;
+
+        container.requestPointerLock();
+    });
+
+    container.addEventListener('pointermove', event => {
+        // Mention flipping sides
+        playerEuler.x += (event.movementY / container.clientHeight) * params.mouseSensitivity * THREE.MathUtils.DEG2RAD;
+        playerEuler.x = THREE.MathUtils.clamp(
+            playerEuler.x,
+            -80 * THREE.MathUtils.DEG2RAD,
+            80 * THREE.MathUtils.DEG2RAD
+        )
+
+        playerEuler.y -= (event.movementX / container.clientWidth) * params.mouseSensitivity * THREE.MathUtils.DEG2RAD;
+    })
+
+    window.addEventListener('keydown', event => {
+        if (event.repeat) return;
+
+        switch (event.code) {
+            case 'KeyW':
+                playerInput.forward = 1;
+                break;
+            case 'KeyS':
+                playerInput.backward = 1;
+                break;
+            case 'KeyA':
+                playerInput.left = 1;
+                break;
+            case 'KeyD':
+                playerInput.right = 1;
+                break;
+            case 'Space':
+                playerInput.jump = 1;
+                break;
+        }
+    })
+
+    window.addEventListener('keyup', event => {
+        if (event.repeat) return;
+
+        switch (event.code) {
+            case 'KeyW':
+                playerInput.forward = 0;
+                break;
+            case 'KeyS':
+                playerInput.backward = 0;
+                break;
+            case 'KeyA':
+                playerInput.left = 0;
+                break;
+            case 'KeyD':
+                playerInput.right = 0;
+                break;
+        }
+    })
+}
+
+const GROUNDED_TIMER_DEFAULT_VALUE = 1;
+// For some reason it's bugged when vertical movement equals 0
+const VERTICAL_MOVEMENT = -0.001;
+
+let groundedTimer = 0;
+let verticalMovement = VERTICAL_MOVEMENT;
+
+function updatePlayer(deltaTimeMs: number): void {
+    // For some reason camera is facing -Z in default, so we need to fix it
+    playerCamera.rotation.set(playerEuler.x, 180 * THREE.MathUtils.DEG2RAD, 0);
+
+    const nextQuaternion = new THREE.Quaternion().setFromEuler(new THREE.Euler(0, playerEuler.y, 0));
+    playerCollider.setRotation(nextQuaternion);
+    playerGroup.quaternion.copy(nextQuaternion); // SYNC WITH THREE.JS
+
+    const movementVector = new THREE.Vector3(
+        playerInput.left - playerInput.right,
+        0,
+        playerInput.forward - playerInput.backward
+    ).normalize().multiplyScalar(PLAYER_SPEED).applyQuaternion(nextQuaternion).divideScalar( deltaTimeMs);
+
+    // This should happen when character is grounded and not sliding?
+    if (characterController.computedGrounded()) {
+        groundedTimer = GROUNDED_TIMER_DEFAULT_VALUE;
+        verticalMovement = VERTICAL_MOVEMENT;
+
+        if (playerInput.jump) {
+            verticalMovement = 0.25;
+            playerInput.jump = 0;
+        }
+    }
+
+    if (groundedTimer > 0) {
+        groundedTimer = Math.max(groundedTimer - (deltaTimeMs / 1000), 0);
+    }
+
+    movementVector.setY(verticalMovement);
+    verticalMovement = Math.max(
+        (verticalMovement + GRAVITY_VECTOR.y * (deltaTimeMs / 1000) * 0.1),
+        GRAVITY_VECTOR.y * (deltaTimeMs / 1000) * 2
+    )
+
+    characterController.computeColliderMovement(playerCollider, movementVector);
+    const currentPosition = toVector3(playerCollider.translation());
+    const computedMovementVector = toVector3(characterController.computedMovement());
+
+    const nextPosition = currentPosition.clone().add(computedMovementVector);
+
+    playerCollider.setTranslation(nextPosition);
+    playerGroup.position.copy(nextPosition); // SYNC WITH THREE.JS
+}
+
 let startTimeMs: number | null = null;
 let elapsedTimeMs: number | null = null;
 let elapsedTickCounter: number = 0;
-
-let playerEuler = new THREE.Euler(0, 0,0);
-
-function player(): void {
-    playerEuler.set(
-
-    )
-}
 
 function animate(timeMs: number): void {
     stats.begin();
@@ -173,11 +339,11 @@ function animate(timeMs: number): void {
     )
 
     for (let tick = elapsedTickCounter; tick < tickCounter; tick++) {
-        animateRapier();
+        updateRapier();
     }
 
     const deltaTimeMs = elapsedTimeMs !== null ? timeMs - elapsedTimeMs : 0;
-    animateThree(deltaTimeMs);
+    updateThree(deltaTimeMs);
 
     stats.end();
 
@@ -188,7 +354,7 @@ function animate(timeMs: number): void {
 
 let debugLineGeometries: THREE.BufferGeometry[] = [];
 
-function animateRapier(): void {
+function updateRapier(): void {
     physicsWorld.step();
 
     // THIS DEBUG CAN BECOME VERY SLOW ON LARGE MESHES
@@ -251,9 +417,13 @@ function getLineMaterial(r: number, g: number, b: number): THREE.LineBasicMateri
     return material;
 }
 
-function animateThree(_: number): void {
+function updateThree(deltaTimeMs: number): void {
     orbitControls.update();
-    renderer.render(scene, freeRoamCamera);
+
+    // PLAYER RELATED STUFF
+    updatePlayer(deltaTimeMs);
+
+    renderer.render(scene, activeCamera);
 }
 
 function clearAnimationAndTimings(): void {
@@ -278,7 +448,16 @@ function onWindowResize(): void {
     freeRoamCamera.aspect = window.innerWidth / window.innerHeight;
     freeRoamCamera.updateProjectionMatrix();
 
+    playerCamera.aspect = window.innerWidth / window.innerHeight;
+    playerCamera.updateProjectionMatrix();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-
+function toVector3({x, y, z}: RAPIER.Vector3): THREE.Vector3 {
+    return new THREE.Vector3(
+        x,
+        y,
+        z
+    )
+}
